47507d906c2906ef89296276fdc31daf7a281e36
Rodolfo <rodogd@gmail.com>
2017-04-02 16:42:07 -0700

    Updates

--- src/Calendarizador.java
+++ src/Calendarizador.java
@@ -6,15 +6,22 @@ import java.util.List;
 public class Calendarizador {
     
     private Proceso[] procesos = Tareas.getProcesos();
-    private Memoria[] bloquesMemoria = new Memoria[10];
+    private Memoria[] bloquesMemoria;
     
     public void inicializarBloquesMemoria()
     {
-        for (int i=0;i<10;i++)
+        int tamanos[] = {9500, 7000, 4500, 8500, 3000, 9000, 1000, 5500, 1500, 500};
+        bloquesMemoria = new Memoria[10];
+        
+        for (int i=0;i<bloquesMemoria.length;i++)
         {
-            if (procesos[i].getEstado().equals(Constantes.INACTIVO))
-            {
-                bloquesMemoria[i] = new Memoria(i, procesos[i]);
+            bloquesMemoria[i] = new Memoria(i, tamanos[i], null);
+            
+            for (Proceso proceso : procesos) {
+                if (tieneEstado(proceso, Constantes.INACTIVO) && cabeEnBloque(proceso, bloquesMemoria[i])) {
+                    bloquesMemoria[i].setProceso(proceso);
+                    break;
+                }
             }
         }
     }
@@ -38,5 +45,15 @@ public class Calendarizador {
         List<Memoria> listaMemoriaOrdenada = Arrays.asList(bloquesMemoria);
         Collections.sort(listaMemoriaOrdenada, comparing((t) -> t.proceso.Tiempo));
         return listaMemoriaOrdenada.toArray(bloquesMemoria);
+    }
+      
+    private boolean tieneEstado(Proceso proceso, String estado)
+    {
+        return proceso.getEstado().equals(estado);
+    }    
+        
+    private boolean cabeEnBloque(Proceso proceso, Memoria bloqueMemoria)
+    {
+        return bloqueMemoria.getTamano() >= proceso.getTamano();
     }
 }
--- src/Memoria.java
+++ src/Memoria.java
@@ -1,14 +1,15 @@
 public class Memoria {
-    
-    public int bloque;
+   
+    public int bloque, tamano;
     public Proceso proceso;
 
     public Memoria() {
 
     } 
-    public Memoria(int bloque, Proceso proceso) {
+    public Memoria(int bloque, int tamano, Proceso proceso) {
         this.bloque = bloque;
         this.proceso = proceso;
+        this.tamano = tamano;
     }  
     
     public int getBloque() {
@@ -16,5 +17,21 @@ public class Memoria {
     } 
     public void setBloque(int Bloque) {
         this.bloque = Bloque;
+    }
+    
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+    
+    public Proceso getProceso() {
+        return proceso;
+    }
+
+    public void setProceso(Proceso proceso) {
+        this.proceso = proceso;
     }
 }
