61ffe302eadd874aca9b2fef3b41e2fc34624355
Rodolfo <rodogd@gmail.com>
2017-04-02 16:53:46 -0700

    Add files via upload

---
+++ Memoria.java
@@ -0,0 +1,86 @@
+public class Memoria {
+<<<<<<< HEAD
+    public int Bloque, Tamano, Proceso, Tiempo; 
+    public boolean used;  
+    public Memoria memoria[]; 
+=======
+   
+    public int bloque, tamano;
+    public Proceso proceso;
+
+>>>>>>> refactor
+    public Memoria() {
+        memoria = new Memoria[10];  
+        memoria[0]=new Memoria(1,9500,7, 0);
+        memoria[1]=new Memoria(2,7000,10, 0); 
+        memoria[2]=new Memoria(3,4500,0, 0); 
+        memoria[3]=new Memoria(4,8500,4, 0);  
+        memoria[4]=new Memoria(5,3000,5, 0);
+        memoria[5]=new Memoria(6,9000,6, 0);
+        memoria[6]=new Memoria(7,1000,8, 0);  
+        memoria[7]=new Memoria(8,5500,9, 0); 
+        memoria[8]=new Memoria(9,1500,14, 0); 
+        memoria[9]=new Memoria(10,500,15, 0);
+    } 
+<<<<<<< HEAD
+    public Memoria(int Bloque, int Tamano, int Proceso, int Tiempo) {
+        this.Bloque = Bloque;
+        this.Tamano = Tamano;
+        this.Proceso = Proceso;
+        this.Tiempo = Tiempo; 
+=======
+    public Memoria(int bloque, int tamano, Proceso proceso) {
+        this.bloque = bloque;
+        this.proceso = proceso;
+        this.tamano = tamano;
+>>>>>>> refactor
+    }  
+    public Memoria getMemoria(int p){
+        return memoria[p];
+    } 
+    public int getBloque() {
+        return Bloque;
+    } 
+    public void setBloque(int Bloque) {
+        this.Bloque = Bloque;
+    }
+
+    public int getTamano() {
+        return Tamano;
+    }
+<<<<<<< HEAD
+
+    public void setTamano(int Tamano) {
+        this.Tamano = Tamano;
+    } 
+    public int getProceso() {
+        return Proceso;
+    } 
+    public void setProceso(int Proceso) {
+        this.Proceso = Proceso;
+    } 
+    public int getTiempo() {
+        return Tiempo;
+    } 
+    public void setTiempo(int Tiempo) {
+        this.Tiempo = Tiempo;
+    }   
+=======
+    
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+    
+    public Proceso getProceso() {
+        return proceso;
+    }
+
+    public void setProceso(Proceso proceso) {
+        this.proceso = proceso;
+    }
+>>>>>>> refactor
+}
