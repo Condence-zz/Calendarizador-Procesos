8e9b5590246df35aa7e5c094cfa4fe5e0007981d
Rodolfo <rodogd@gmail.com>
2017-04-02 16:47:55 -0700

    Revert "Refactor code"

--- src/Calendarizador.java
+++ src/Calendarizador.java
@@ -1,42 +1,6 @@
-import java.util.Arrays;
-import java.util.Collections;
-import static java.util.Comparator.comparing;
-import java.util.List;
-
 public class Calendarizador {
-    
-    private Proceso[] procesos = Tareas.getProcesos();
-    private Memoria[] bloquesMemoria = new Memoria[10];
-    
-    public void inicializarBloquesMemoria()
-    {
-        for (int i=0;i<10;i++)
-        {
-            if (procesos[i].getEstado().equals(Constantes.INACTIVO))
-            {
-                bloquesMemoria[i] = new Memoria(i, procesos[i]);
-            }
-        }
-    }
-  
+    Tareas tareas = new Tareas();
     public void cambiarEstado(int i, String estado){
-        Tareas.getProceso(i).setEstado(estado);
-    }
-    
-    public Proceso[] getProcesos()
-    {
-        return procesos;
-    }
-    
-    public Memoria[] getBloquesMemoria()
-    {
-        return bloquesMemoria;
-    }
-    
-        public Memoria[] getBloquesMemoriaOrdenados()
-    {
-        List<Memoria> listaMemoriaOrdenada = Arrays.asList(bloquesMemoria);
-        Collections.sort(listaMemoriaOrdenada, comparing((t) -> t.proceso.Tiempo));
-        return listaMemoriaOrdenada.toArray(bloquesMemoria);
+        tareas.getProceso(i).setEstado(estado);
     }
 }
