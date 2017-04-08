5a1884dd65c06c678d8d00bf5b51ea142ce27fda
Rodolfo <rodogd@gmail.com>
2017-04-07 19:44:08 -0700

    Updates

--- src/Calendarizador.java
+++ src/Calendarizador.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import static java.util.Comparator.comparing;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Calendarizador {
     
-    private Proceso[] procesos = Tareas.getProcesos();
+    private final Proceso[] procesos = Tareas.getProcesos();
     private Memoria[] bloquesMemoria;
+    private int procesosTotalesTerminados = 0;
     
     public void inicializarBloquesMemoria()
     {
@@ -18,16 +22,118 @@ public class Calendarizador {
             bloquesMemoria[i] = new Memoria(i, tamanos[i], null);
             
             for (Proceso proceso : procesos) {
-                if (tieneEstado(proceso, Constantes.INACTIVO) && cabeEnBloque(proceso, bloquesMemoria[i])) {
+                if (comparaEstado(proceso.getEstado(), Constantes.INACTIVO) &&
+                    cabeEnBloque(proceso.getTamano(), bloquesMemoria[i].getTamano()))
+                {
+                    proceso.setEstado(Constantes.ASIGNADO);
                     bloquesMemoria[i].setProceso(proceso);
                     break;
                 }
             }
         }
     }
+    
+    public void procesar()
+    {
+        try 
+        {
+            int procesosTerminados = 0;
+            
+            for (Memoria memoria : bloquesMemoria)
+            {
+                if (procesosTotalesTerminados == 25)
+                {
+                    System.out.println("TERMINADO.");
+                    return;
+                }
+                
+                Thread.sleep(500);
+                
+                if (procesosTerminados == 2)
+                {
+                    System.out.println("Reordenando...");
+                    Thread.sleep(1000);
+                    break;
+                }
+
+                int procesando = 0;
+                procesando = memoria.proceso.getNum();
+                
+                System.out.println("=====================================");
+                System.out.println("Procesando: " + procesando);
+                
+                procesosTerminados++;
+                procesosTotalesTerminados++;
+                
+                Proceso procesoSiguiente = getSiguienteProceso(procesando);
+                    
+                if (procesoSiguiente != null)
+                {
+                    if (procesosTerminados == 2 || procesosTotalesTerminados == 25)
+                    {
+                        System.out.println("Siguiente: ninguno");
+                    }
+                    else
+                    {
+                        System.out.println("Siguiente: " + procesoSiguiente.getNum());
+                    }
+                } 
+                
+                memoria.proceso = null;
+                terminarProceso(procesando);
+                rellenarBloque();
+                
+                System.out.println("Total procesados: " + procesosTotalesTerminados); 
+            }
+        }
+        catch (InterruptedException ex) {
+            Logger.getLogger(Calendarizador.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        
+        bloquesMemoria = getBloquesMemoriaOrdenados();
+        procesar();
+    }
   
-    public void cambiarEstado(int i, String estado){
-        Tareas.getProceso(i).setEstado(estado);
+    private void terminarProceso(int id)
+    {
+        for(Proceso proceso : procesos)
+        {
+            if(proceso == null || proceso.Num != id)
+            {
+                continue;
+            }
+            proceso.setEstado(Constantes.TERMINADO);
+            return;
+        }
+    }
+    private void rellenarBloque()
+    {
+        for(Memoria memoria : bloquesMemoria)
+        {
+            if (memoria == null)
+            {
+                continue;
+            }
+            
+            if (memoria.proceso != null)
+            {
+                continue;
+            }
+            
+            for(Proceso proceso : procesos)
+            {
+                if (comparaEstado(proceso.getEstado(), Constantes.EN_ESPERA) ||
+                    comparaEstado(proceso.getEstado(), Constantes.INACTIVO) &&
+                    cabeEnBloque(proceso.getTamano(), memoria.getTamano()))
+                {
+                    proceso.setEstado(Constantes.ASIGNADO);
+                    memoria.proceso = proceso;
+                    System.out.println("==================== \n " +
+                                       "Asignado el proceso: " + proceso.toString() + " en: " + memoria.toString());
+                    return;
+                }
+            }
+        }
     }
     
     public Proceso[] getProcesos()
@@ -40,20 +146,86 @@ public class Calendarizador {
         return bloquesMemoria;
     }
     
-        public Memoria[] getBloquesMemoriaOrdenados()
+    public Proceso getSiguienteProceso(int numero)
+    {
+        for(int i=0; i<bloquesMemoria.length; i++)
+        {
+            if(bloquesMemoria[i] == null)
+            {
+                continue;
+            }
+            
+            if(bloquesMemoria[i].proceso == null)
+            {
+                continue;
+            }
+            
+            if(bloquesMemoria[i].proceso.getNum() != numero)
+            {
+                continue;
+            }
+            
+            if(bloquesMemoria.length == i && bloquesMemoria[i+1].proceso != null)
+            {
+                continue;
+            }
+            
+            return bloquesMemoria[i+1].proceso;
+
+        }
+        
+        return null;
+    }
+    
+    public void limpiarBloque(int bloque)
+    {
+        for(int i=0;i<bloquesMemoria.length;i++)
+        {
+            if (bloquesMemoria[i] != null && bloquesMemoria[i].bloque == bloque)
+            {
+                bloquesMemoria[i] = null;
+                return;
+            }
+        }
+    }
+    
+    public Memoria getBloquePorProceso(int idProceso)
+    {
+        if (bloquesMemoria == null || bloquesMemoria.length == 0)
+        {
+            return null;
+        }
+        for(Memoria bloqueMemoria : bloquesMemoria)
+        {
+            if (bloqueMemoria.proceso.Num == idProceso)
+            {
+                return bloqueMemoria;
+            }
+        }
+        return null;
+    }
+    
+    public Memoria[] getBloquesMemoriaOrdenados()
     {
         List<Memoria> listaMemoriaOrdenada = Arrays.asList(bloquesMemoria);
-        Collections.sort(listaMemoriaOrdenada, comparing((t) -> t.proceso.Tiempo));
-        return listaMemoriaOrdenada.toArray(bloquesMemoria);
+        List<Memoria> listaMemoriaSinVacios = new ArrayList<Memoria>();
+        for (Memoria memoria : listaMemoriaOrdenada) {
+            if (memoria != null && memoria.proceso != null)
+            {
+                listaMemoriaSinVacios.add(memoria);
+            }
+        }
+        Collections.sort(listaMemoriaSinVacios, comparing((t) -> t.proceso.Tiempo));
+        return listaMemoriaSinVacios.toArray(bloquesMemoria);
     }
       
-    private boolean tieneEstado(Proceso proceso, String estado)
+    private boolean comparaEstado(String estadoActual, String estadoEsperado)
     {
-        return proceso.getEstado().equals(estado);
+        return estadoActual.equals(estadoEsperado);
     }    
         
-    private boolean cabeEnBloque(Proceso proceso, Memoria bloqueMemoria)
+    private boolean cabeEnBloque(int tamanoProceso, int tamanoMemoria)
     {
-        return bloqueMemoria.getTamano() >= proceso.getTamano();
+        return tamanoProceso <= tamanoMemoria;
     }
 }
--- src/main.java
+++ src/main.java
@@ -2,28 +2,17 @@ public class main{
     public static void main(String args[]){
         Calendarizador calendarizador = new Calendarizador();
         
-        System.out.println("Tareas");
-
-        for(Proceso proceso : calendarizador.getProcesos())
-        {
-            System.out.println(proceso.getEstado());
-        }
-        
+        // Llenar la memoria con procesos
         calendarizador.inicializarBloquesMemoria();
         
-        System.out.println("Lista de procesos asignados a memoria");
+        // Comenzar el procesado
+        calendarizador.procesar();
         
-        for(Memoria memoria : calendarizador.getBloquesMemoria())
+        System.out.println("Lista de procesos:");
+        
+        for(Proceso proceso : calendarizador.getProcesos())
         {
-            System.out.println(memoria.proceso.getTiempo());
+            System.out.println(proceso.toString());
         }
-        
-        System.out.println("Lista ordenada de procesos asignados a memoria");
-        
-        for(Memoria memoria : calendarizador.getBloquesMemoriaOrdenados())
-        {
-            System.out.println(memoria.proceso.getTiempo());
-        }
-        
     }
 }
